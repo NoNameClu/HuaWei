dcac7058f037d1d30de5d1c196f400f6100fa200
Darkericy <78835438+Darkericy@users.noreply.github.com>
2023-03-13 22:23:58 +0800

    优化框架

--- Command.cpp
+++ Command.cpp
@@ -1,5 +1,7 @@
 #include "Command.h"
 
+const unordered_set<int> Command::style{ 1,2,3,4,5,6,7,8,9 };
+
 void Command::init()
 {
 	ReadUntilOK();
@@ -48,10 +50,69 @@ void Command::initMap()
 	for (int i = 0; i < buf.size(); ++i) {
 		for (int j = 0; j < buf[0].size(); ++j) {
 			if (buf[i][j] == 'A') {
-
+				robot temp;
+				temp.state = NONE;
+				robots.push_back(temp);
+			}
+			if (style.find(buf[i][j] - '0') != style.end()) {
+				worker temp;
+				pair<int, int> pos = make_pair(i, j);
+				pair<double, double> real_pos;
+				mapToreal(pos, real_pos);
+				temp.pos = pos;
+				temp.real_pos = real_pos;
+				temp.style = buf[i][j] - '0';
+				switch (temp.style) {
+				case 1:
+					temp.need_money = 3000;
+					temp.sell_money = 6000;
+					break;
+				case 2:
+					temp.need_money = 4400;
+					temp.sell_money = 7600;
+					break;
+				case 3:
+					temp.need_money = 5800;
+					temp.sell_money = 9200;
+					break;
+				case 4:
+					temp.need_money = 15400;
+					temp.sell_money = 22500;
+					temp.need_object |= (OBJECT_ONE | OBJECT_TWO);
+					break;
+				case 5:
+					temp.need_money = 17200;
+					temp.sell_money = 25000;
+					temp.need_object |= (OBJECT_ONE | OBJECT_THREE);
+					break;
+				case 6:
+					temp.need_money = 19200;
+					temp.sell_money = 27500;
+					temp.need_object |= (OBJECT_TWO | OBJECT_THREE);
+					break;
+				case 7:
+					temp.need_money = 76000;
+					temp.sell_money = 105000;
+					temp.need_object |= (OBJECT_FOUR | OBJECT_FIVE | OBJECT_SIX);
+					break;
+				case 8:
+					temp.need_money = 0;
+					temp.sell_money = 0;
+					temp.need_object |= (OBJECT_SEVEN);
+					break;
+				case 9:
+					temp.need_money = 0;
+					temp.sell_money = 0;
+					temp.need_object |= (OBJECT_ONE | OBJECT_TWO | OBJECT_THREE | OBJECT_FOUR | OBJECT_FIVE | OBJECT_SIX | OBJECT_SEVEN);
+				default:
+					break;
+				}
+				idToworker[i * 100 + j] = temp;
 			}
 		}
 	}
+
+	//Ñ­»·ÅÐ¶Ï¼ÆËãÂ·¾¶
 }
 
 void Command::UpdateInfo()
--- Command.h
+++ Command.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <unordered_map>
+#include <unordered_set>
 #include <list>
 #include <vector>
 
@@ -15,33 +16,49 @@ const int OBJECT_SIX = 1 << 6;
 const int OBJECT_SEVEN = 1 << 7;
 
 enum robot_state {
+	//添加none状态，表示现在没有分配工作
+	NONE,			
 	BEFORE,
 	AFTER
 };
 
 struct worker {
 	int style;
+	int hold_object;
+	int need_object;					//添加need，表示所需要的，上面的表示持有的
+	int need_money, sell_money;			//该工作台制造的物品的买价格和售价格
+
 	pair<int, int> pos;
 	pair<double, double> real_pos;
-	int hold_object;
+
+	worker() : hold_object(0) {};
 };
 
 struct route {
 	int start, end;
-	int base;		//所需的最小钱数
-	int value;
+	int base;					//所需的最小钱数
+	int value;					//这里一开始就要做归一化操作
 	int length;
 	int start_object, end_object;
+
+	route() {};
+	route(int s, int e) : start(s), end(e) {};
 };
 
 struct robot {
 	bool on_job;
-	pair<int, int> object_target;
-	double a_speed;
-	pair<double, double> l_speed;
 	bool can_buy, can_sell;
+	pair<int, int> object_target;
 	robot_state state;
 	route cur;
+	
+	double a_speed;						//角速度
+	pair<double, double> l_speed;		//线速度
+	pair<double, double> real_pos;		//机器人的当前坐标，每一帧读取的时候要改变
+
+	robot() : on_job(false), can_buy(false), can_sell(false),
+		state(NONE), a_speed(0), l_speed(make_pair(0, 0)) {};
+
 };
 
 class Command
@@ -59,6 +76,8 @@ class Command
 	vector<robot> robots;					//机器人数组
 	vector<string> buf, response;			//读入缓冲区，输出缓冲区
 	unordered_map<int, worker> idToworker;	//id，到worker的索引，12,13,1213。 45.75 49.25   (x - 0.25) / 0.5
+
+	const static unordered_set<int> style;	//添加这个常量，主要用在读地图操作的时候
 
 	bool ReadUntilOK();		
 	void Clear();
